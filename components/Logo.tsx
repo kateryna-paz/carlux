8a4c4378152fb106be754b7dacc8b054bcb539ef
Kateryna Paz <katepaz05@gmail.com>
2025-07-03 13:35:22 +0300

    v3

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <Image
-      src="/images/logo.png"
-      alt="Logo"
-      width={200}
-      height={100}
-      className="w-[130px] h-auto lg:w-[180px] md:w-[150px]"
-    />
+    <Link href={"/"}>
+      <Image
+        src="/images/logo.png"
+        alt="Logo"
+        width={200}
+        height={100}
+        className="w-[120px] h-auto lg:w-[180px] md:w-[150px]"
+      />
+    </Link>
   );
 }
 
